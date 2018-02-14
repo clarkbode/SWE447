3b429df4484b50a08dac9c4949aaf60a005e38ad
clarkbode <35516752+clarkbode@users.noreply.github.com>
2018-02-13 20:24:11 -0800

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -65,7 +65,7 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     0,  1,  2,      0,  2,  3,    // front
     4,  5,  6,      4,  6,  7,    // back
     8,  9,  10,     8,  10, 11,   // top
-    12, 13, 14,     12, 14, 15,   // bottom
+    4, 13, 14,     4, 14, 15,   // bottom
     16, 17, 18,     16, 18, 19,   // right
     20, 21, 22,     20, 22, 23,   // left    
         ])
