aeb027460b0efc8ae8b3ef70c406d3bc3627d8fe
clarkbode <35516752+clarkbode@users.noreply.github.com>
2018-02-20 18:47:35 -0800

    Update SolarSystem.js

--- Assignment-5/SolarSystem.js
+++ Assignment-5/SolarSystem.js
@@ -19,8 +19,8 @@ var SolarSystem = {
   },
   Earth : {
     radius : 1,
-    distance : 10,
-    year : 10,
+    distance : 1*10,
+    year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Moon : {
